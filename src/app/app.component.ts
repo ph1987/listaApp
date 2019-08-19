b234d9513381f8733362ad1b2fc364762bd51a13
ph1987 <ph1987@gmail.com>
2019-08-19 03:27:05 -0300

    leitura do JSON Ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'lista';
+
+export class AppComponent implements OnInit {
+  title = 'lista rock';
+  ngOnInit() {
+    var json = require('/home/phil/Documentos/dev/python/listaBot/events.json');
+    console.log(json);
+  }
 }
